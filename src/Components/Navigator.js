e55346fe6c253732c08bc988c350926390e80a76
riad <riad@gmail.com>
2022-05-11 14:44:57 +0100

    c

--- src/Components/Navigator.js
+++ src/Components/Navigator.js
@@ -6,29 +6,36 @@ import axios from 'axios'
 
 import { useEffect, useState } from 'react';
 var rootdirectoriesUrl = "http://localhost:8081/manager/v1/users_home_directories"
-const dd = [{ "id": 41, "name": "riadHome", "owner": "riad", "parentFolder": { "id": 40, "name": "root", "owner": "all", "parentFolder": null, "taille": 0, "hibernateLazyInitializer": {} }, "taille": 0 }, { "id": 42, "name": "medHome", "owner": "med", "parentFolder": { "id": 40, "name": "root", "owner": "all", "parentFolder": null, "taille": 0, "hibernateLazyInitializer": {} }, "taille": 0 }]
+// const dd = [{ "id": 41, "name": "riadHome", "owner": "riad", "parentFolder": { "id": 40, "name": "root", "owner": "all", "parentFolder": null, "taille": 0, "hibernateLazyInitializer": {} }, "taille": 0 }, { "id": 42, "name": "medHome", "owner": "med", "parentFolder": { "id": 40, "name": "root", "owner": "all", "parentFolder": null, "taille": 0, "hibernateLazyInitializer": {} }, "taille": 0 }]
 function Navigator() {
-
     const [rootDirName, setRootDirName] = useState([]);
     const [error, setError] = useState(false);
     const [parentDirectory,setParentDirectory]=useState(['root',40])
     const [username,setUsername]=useState("all")
     const [parent,setParent]=useState(41)
-    const goBack=(username)=>{
-        axios.get(`http://localhost:8081/manager/v1/folder/${username}/${parentDirectory[1]}`,{
-        method: 'HEAD',
-        mode: 'no-cors',
-        headers: {
-            'Access-Control-Allow-Origin': '*',
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
-        }})
-        .then(res=>
-                    setRootDirName(res.data)
-            )
-    }
-    useEffect(() => {
-
+    const openFolder=(username,id)=>{
+        const init = {
+          mode: 'no-cors',
+          methode: 'GET'
+         }
+      axios.get(`http://localhost:8081/manager/v1/folder/${username}/${id}`,{
+          method: 'HEAD',
+          mode: 'no-cors',
+          headers: {
+              'Access-Control-Allow-Origin': '*',
+              Accept: 'application/json',
+              'Content-Type': 'application/json',
+          },
+          crossdomain: true,
+      })
+      .then(
+          res=>{
+              console.log(res.data)
+              setRootDirName(res.data);
+      }
+      )}
+    const goBack=(username)=> openFolder(username,parentDirectory[1])
+    const getHomeDirectories=()=>{
         const init = {
             mode: 'no-cors',
             methode: 'GET'
@@ -40,18 +47,24 @@ function Navigator() {
             mode: 'no-cors',
             headers: {
                 'Access-Control-Allow-Origin': '*',
-                Accept: 'application/json',
+                'Accept': 'application/json',
                 'Content-Type': 'application/json',
             },
             crossdomain: true,
         })
          .then(
-             res=>setRootDirName(res.data)
-
+             res=>{
+                 setRootDirName(res.data)
+                 setParentDirectory([rootDirName.parentFolder.name,rootDirName.parentFolder.id])
+             }
          )
-     
-        //  setParentDirectory(['root',0])
-        //  setUsername("all")
+    }
+
+
+    
+    useEffect(() => {
+       getHomeDirectories();
+        
     }, [])
 
 
